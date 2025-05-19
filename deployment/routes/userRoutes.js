bd98c0aced5941917ed52daa390a79ac3ad598af
amd9575 <perdu.trouve.objet@gmail.com>
2025-05-19 15:06:21 +0200

    MAJ2 USER CREATION

--- deployment/routes/userRoutes.js
+++ deployment/routes/userRoutes.js
@@ -7,5 +7,10 @@ router.post('/update-device-token', userController.updateDeviceToken);
 router.post('/create', userController.createUser);
 router.post('/register', userController.registerUser);
 
+router.get('/ping', (req, res) => {
+  res.send('pong depuis /api/users/ping');
+});
+
+
 module.exports = router;
 
