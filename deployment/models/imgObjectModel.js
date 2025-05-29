3a752fe60feed4d51e1d5279c484492dafdbe51a
amd9575 <perdu.trouve.objet@gmail.com>
2025-05-29 12:56:05 +0200

    MAJ3 CORRECTION NOM TABLE IMAGE_OBJECT

--- deployment/models/imgObjectModel.js
+++ deployment/models/imgObjectModel.js
@@ -3,9 +3,9 @@ const db = require('../db');
 // ➕ Ajouter une image
 const insertImage = async (img) => {
   const query = `
-    INSERT INTO image_object (img_name, pHash, id_object)
+    INSERT INTO image_object (imgobject_name, imgobject_phash, _id_object)
     VALUES ($1, $2, $3)
-    RETURNING _id_img
+    RETURNING _id_imgobject
   `;
   const values = [img.imgName, img.pHash, img.id_object];
 
@@ -21,14 +21,14 @@ const getAllImages = async () => {
 
 // 📥 Récupérer les images liées à un objet
 const getImagesByObjectId = async (objectId) => {
-  const query = `SELECT * FROM image_object WHERE id_object = $1`;
+  const query = `SELECT * FROM image_object WHERE _id_object = $1`;
   const result = await db.query(query, [objectId]);
   return result.rows;
 };
 
 // ❌ Supprimer une image
 const deleteImage = async (id) => {
-  const query = `DELETE FROM image_object WHERE _id_img = $1`;
+  const query = `DELETE FROM image_object WHERE _id_imgobject = $1`;
   await db.query(query, [id]);
 };
 
@@ -36,10 +36,10 @@ const deleteImage = async (id) => {
 const updateImage = async (id, data) => {
   const query = `
     UPDATE image_object SET
-      img_name = $1,
-      pHash = $2,
-      id_object = $3
-    WHERE _id_img = $4
+      imgobject_name = $1,
+      imgobject_phash = $2,
+      _id_object = $3
+    WHERE _id_imgobject = $4
     RETURNING *
   `;
   const values = [data.imgName, data.pHash, data.id_object, id];
