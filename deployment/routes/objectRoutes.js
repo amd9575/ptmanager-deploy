d46bcaa3f8938884c20bf711eb5feef4f8b6b820
amd9575 <perdu.trouve.objet@gmail.com>
2025-06-09 15:37:14 +0200

    CORRECTIF3 SUITE DEPLOY AVEC ERREUR

--- deployment/routes/objectRoutes.js
+++ deployment/routes/objectRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 const objectController = require('../controllers/objectController');
 
 router.post('/', objectController.createObject);
